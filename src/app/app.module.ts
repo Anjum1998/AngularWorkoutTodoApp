6fd1d753f1d1b731ae327b8eb143787588ed5e6b
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-11-29 22:16:05 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ViewTodoComponent } from './view-todo/view-todo.component';
+import { HttpClientModule } from '@angular/common/http';
+const myRoute:Routes=[
+  {
+    path:"",
+    component:ViewTodoComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewTodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute),
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
